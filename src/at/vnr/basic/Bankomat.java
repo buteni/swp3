250072f613a0af482e18998efcabd90a30f3767f
buteni <valentin.nuessler@student.htldornbirn.at>
2023-10-10 17:43:47 +0200

    Package basics mit bsp erstellt

---
+++ src/at/vnr/basic/Bankomat.java
@@ -0,0 +1,63 @@
+package at.vnr.basic;
+
+import java.util.Scanner;
+
+public class Bankomat {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean Abbruch = true;
+        double dKontostand = 0;
+
+        while(Abbruch) {
+
+
+
+            System.out.println("1. Einzahlen");
+            System.out.println("2. Abheben");
+            System.out.println("3. Kontostand");
+            System.out.println("4. Abbrechen");
+
+            int Auswahl = scanner.nextInt();
+
+
+            if (Auswahl == 1) {
+
+                System.out.println("Geben Sie ein, wieviel Sie einzahlen wollen:");
+                double betrag = scanner.nextDouble();
+                dKontostand += betrag;
+                System.out.println("Sie haben " + betrag + " Euro eingezahlt");
+
+            } else if (Auswahl == 2) {
+
+                System.out.println("Geben Sie ein, wieviel Sie abheben wollen:");
+                double betrag = scanner.nextDouble();
+
+                if (betrag <= dKontostand) {
+                    dKontostand = dKontostand - betrag;
+                    System.out.println("Sie haben " + betrag + " Euro abgehoben");
+
+                } else {
+                    System.out.println("Sie haben zu wenig Geld auf dem Konto.");
+                }
+
+            } else if (Auswahl == 3) {
+
+                System.out.println("Ihr Kontostand beträgt: " + dKontostand);
+            } else if (Auswahl == 4) {
+
+                Abbruch = false;
+
+                System.out.println("Sie werden abgemeldet");
+            } else {
+
+                System.out.println("Falsche Eingabe!");
+            }
+
+
+        }
+
+
+    }
+}
--- src/at/vnr/basic/ControlStructurs8.java
+++ src/at/vnr/basic/ControlStructurs8.java
@@ -5,25 +5,26 @@ import java.util.Random;
 public class ControlStructurs8 {
     public static void main(String[] args) {
 
-        //Erstelle ein Programm, dass Zufallszahlen zwischen 10 und 30 generiert. Das Programm soll die Zufallszahlen zusammenzählen.
-        // Sobald die Zahl 15 oder die Zahl 25 kommt, wird das Programm beendet und die Summe der vorherigen Zufallszahlen ausgegeben!
-
         Random random = new Random();
 
-        int randomNumber = random.nextInt(10,30);
+        boolean Abbruch = false;
+        int iSumme = 0;
 
+        while(!Abbruch) {
 
+            int randomNumber = random.nextInt(10, 30);
 
-        System.out.println(randomNumber);
+            System.out.println(randomNumber);
 
+            if((randomNumber == 15) || (randomNumber == 25) ){
+                Abbruch = true;
+            }
 
+            iSumme = iSumme + randomNumber;
+        }
 
-
-
-
-
-
-
+        System.out.println("Die Summer der vorherigen Zahlen lautet:");
+        System.out.println(iSumme);
 
 
 
@@ -32,4 +33,5 @@ public class ControlStructurs8 {
 
 
     }
+
 }
